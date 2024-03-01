6107e9194d0ee58b0344c9662bc4910088a1acb2
Mohamed <afandimohammad362@gmail.com>
2024-03-01 17:03:14 +0100

    Servce server

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -22,7 +22,8 @@ setup(
         'console_scripts': [
             "py_node = my_py_pkg.my_first_node:main",
             "number_publisher= my_py_pkg.number_publisher:main",
-            "number_counter=my_py_pkg.number_counter:main"
+            "number_counter=my_py_pkg.number_counter:main",
+            "add_two_ints_server=my_py_pkg.add_two_ints_server:main"
         ],
     },
 )
