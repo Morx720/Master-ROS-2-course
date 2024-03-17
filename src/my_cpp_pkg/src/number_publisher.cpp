a40b9459f83a38b2369287135e3351cae9a123d1
Mohamed <afandimohammad362@gmail.com>
2024-03-17 16:27:16 +0100

    ros param

--- src/my_cpp_pkg/src/number_publisher.cpp
+++ src/my_cpp_pkg/src/number_publisher.cpp
@@ -1,28 +1,35 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/int64.hpp"
 
-
-class NumberPubNode : public rclcpp::Node 
+class NumberPubNode : public rclcpp::Node
 {
 public:
-    NumberPubNode() : Node("number_publisher") 
+    NumberPubNode() : Node("number_publisher")
     {
-        publisher_ = this -> create_publisher<example_interfaces::msg::Int64>("number", 10);
-        timer_ = this -> create_wall_timer(std::chrono::milliseconds(500),std::bind(&NumberPubNode::timerCallback, this));
+        this->declare_parameter("number_to_publish", 2);
+        this->declare_parameter("publish_rate", 1.0);
+
+        number_ = this->get_parameter("number_to_publish").as_int();
+        double publish_rate = this->get_parameter("publish_rate").as_double();
+
+        publisher_ = this->create_publisher<example_interfaces::msg::Int64>("number", 10);
+        timer_ = this->create_wall_timer(std::chrono::milliseconds((int) (1000.0 / publish_rate)),
+                                         std::bind(&NumberPubNode::timerCallback, this));
         RCLCPP_INFO(this->get_logger(), "number_publisher has been started");
     }
 
 private:
-
-    void timerCallback(){
+    void timerCallback()
+    {
         auto msg = example_interfaces::msg::Int64();
-        msg.data = 2;
+        msg.data = number_;
         publisher_->publish(msg);
-    };
+    }
+
+    int number_;
     rclcpp::Publisher<example_interfaces::msg::Int64>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
-
 
 int main(int argc, char **argv)
 {
--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -6,13 +6,20 @@ from example_interfaces.msg import Int64
 class NumberPublisherNode(Node):
     def __init__(self):
         super().__init__("number_publisher")
+        self.declare_parameter("number_to_publish", 2)
+        self.declare_parameter("publish_rate", 1.0)
+        
+        
+        self.number = self.get_parameter("number_to_publish").value
+        self.publish_rate = self.get_parameter("publish_rate").value
+        
         self.publisher_ = self.create_publisher(Int64,"number",10)
-        timer_ = self.create_timer(0.5,self.timerCallback)
+        timer_ = self.create_timer(1.0 / self.publish_rate ,self.timerCallback)
         self.get_logger().info("number_publisher has been started")
 
     def timerCallback(self):
         msg = Int64()
-        msg.data = 2
+        msg.data = self.number
         self.publisher_.publish(msg)
 
 def main(args=None):
